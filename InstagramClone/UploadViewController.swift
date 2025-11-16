d4f999ae4274971602dd4d0995304eb577cff3ba
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-16 14:57:52 +0300

    user can select image on upload view

--- InstagramClone/UploadViewController.swift
+++ InstagramClone/UploadViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentTextField: UITextField!
@@ -15,11 +15,23 @@ class UploadViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(recognizer)
     }
     
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+    }
 
     @IBAction func uploadButtonClicked(_ sender: Any) {
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
 }
