b54af07595d0e8155252a534c96ee9ef11f24ae1
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-16 17:35:23 +0300

    add alert for upload to storage

--- InstagramClone/UploadViewController.swift
+++ InstagramClone/UploadViewController.swift
@@ -37,12 +37,12 @@ class UploadViewController: UIViewController, UINavigationControllerDelegate, UI
             let imageReference = mediaFolder.child("\(uuid).jpg")
             imageReference.putData(data, metadata: nil) { metadata, error in
                 if error != nil {
-                    print(error?.localizedDescription)
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
                 } else {
                     imageReference.downloadURL { url, error in
                         if error == nil {
                             let imageUrl = url?.absoluteString
-                            print(imageUrl)
+                            //DATABASE
                         }
                     }
                 }
@@ -53,5 +53,12 @@ class UploadViewController: UIViewController, UINavigationControllerDelegate, UI
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true)
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
 }
