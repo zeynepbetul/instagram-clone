45cf717ff88002e19ab3c61794feebf6b3cf2f11
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-16 14:41:34 +0300

    add view elements to the upload view controller

--- InstagramClone/UploadViewController.swift
+++ InstagramClone/UploadViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class UploadViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +19,7 @@ class UploadViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func uploadButtonClicked(_ sender: Any) {
     }
-    */
 
 }
