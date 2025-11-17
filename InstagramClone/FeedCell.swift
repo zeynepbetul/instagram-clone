18779150457dcaefe7ccf7024b6b8f62008a8dce
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-17 22:00:39 +0300

    user can like posts

--- InstagramClone/FeedCell.swift
+++ InstagramClone/FeedCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedCell: UITableViewCell {
 
@@ -13,6 +14,7 @@ class FeedCell: UITableViewCell {
     @IBOutlet weak var likeCountLabel: UILabel!
     @IBOutlet weak var commentLabel: UILabel!
     @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var documentIdLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,5 +28,10 @@ class FeedCell: UITableViewCell {
     }
 
     @IBAction func likeButton(_ sender: Any) {
+        let fireStoreDatabase = Firestore.firestore()
+        if let likeCount = Int(likeCountLabel.text!) {
+            let likeStore = ["likes" : likeCount+1] as [String : Any]
+            fireStoreDatabase.collection("Posts").document(documentIdLabel.text!).setData(likeStore, merge: true)
+        }
     }
 }
--- InstagramClone/FeedViewController.swift
+++ InstagramClone/FeedViewController.swift
@@ -16,6 +16,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     var userCommentArray = [String]()
     var likeArray = [Int]()
     var userImageArray = [String]()
+    var documentIdArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,9 +39,12 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
                     self.userEmailArray.removeAll(keepingCapacity: false)
                     self.userCommentArray.removeAll(keepingCapacity: false)
                     self.likeArray.removeAll(keepingCapacity: false)
+                    self.documentIdArray.removeAll(keepingCapacity: false)
                     
                     for document in snapshot!.documents {
                         let documentID = document.documentID
+                        self.documentIdArray.append(documentID)
+                        
                         if let postedBy = document.get("postedBy") as? String {
                             self.userEmailArray.append(postedBy)
                         }
@@ -70,6 +74,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.likeCountLabel.text = String(likeArray[indexPath.row])
         cell.commentLabel.text = userCommentArray[indexPath.row]
         cell.userImageView.sd_setImage(with: URL(string: self.userImageArray[indexPath.row]))
+        cell.documentIdLabel.text = documentIdArray[indexPath.row]
         return cell
     }
 
